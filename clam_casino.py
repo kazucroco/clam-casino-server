26c727ecf7fafcbcc5a15eb1c432cb443fe00aac
kazucroco <kazu@croco.dev>
2025-06-21 18:48:52 -0400

    Move flip LUT and remaining count into board state

--- clam_casino.py
+++ clam_casino.py
@@ -3,27 +3,20 @@ from game_board import GameBoard
 class ClamCasino:
     level = 0
     score = 0
-    remaining_cards = None
     board = None
     over = False
-    flipped_lut = [[0, 0, 0, 0, 0],
-                   [0, 0, 0, 0, 0],
-                   [0, 0, 0, 0, 0],
-                   [0, 0, 0, 0, 0],
-                   [0, 0, 0, 0, 0]]
     
     def __init__(self, level, size = 5):
         self.level = level
         self.board = GameBoard(self.level, size)
-        self.remaining_cards = self.board.special_count
 
     def flip(self, row, col):
         # cancel the flip if the card is already flipped or game is over
-        if self.over or self.flipped_lut[row][col] == 1:
+        if self.over or self.board.flip_lut[row][col] == 1:
             return -1
         
         # mark the card as flipped on the LUT
-        self.flipped_lut[row][col] = 1
+        self.board.flip_lut[row][col] = 1
 
         # retreive card value
         value = self.board.board[row][col]
@@ -39,10 +32,10 @@ class ClamCasino:
 
         # update special card count
         if value > 1:
-            self.remaining_cards -= 1
+            self.board.special_count -= 1
 
         # end game if all special cards are turned
-        if self.remaining_cards <= 0:
+        if self.board.special_count <= 0:
             self._end_game()
 
         return value
--- game_board.py
+++ game_board.py
@@ -2,8 +2,7 @@ import random
 
 class GameBoard:
     board = None
-    level = 0
-    size = 5
+    flip_lut = None
     special_count = 0
 
     # DIFFICULTY_TABLE[LEVEL][VARIANT][0 | 2x | 3x]
@@ -17,33 +16,27 @@ class GameBoard:
                          [[10, 0, 7], [10, 8, 2], [10, 5, 4], [10, 2, 6], [10, 7, 3]]]
 
     def __init__(self, level = 0, size = 5):
-        self.level = level
-        self.size = size
-        self._generate_values()
-
-    def __str__(self):
-        return str(self.board)
-
-    def _generate_values(self):
         # select variant 0-4
         variant = random.randrange(0, 5)
 
         # table of all values
         self.board = []
+        self.flip_lut = []
 
         # create SIZE rows in the value table
-        for i in range(0, self.size):
+        for i in range(0, size):
             self.board.append([])
+            self.flip_lut.append([0] * size)
 
         # insert 0, 2, and 3 cards
-        self._insert_special(0, self._DIFFICULTY_TABLE[self.level][variant][0])
-        self._insert_special(2, self._DIFFICULTY_TABLE[self.level][variant][1])
-        self._insert_special(3, self._DIFFICULTY_TABLE[self.level][variant][2])
+        self._insert_special(0, self._DIFFICULTY_TABLE[level][variant][0])
+        self._insert_special(2, self._DIFFICULTY_TABLE[level][variant][1])
+        self._insert_special(3, self._DIFFICULTY_TABLE[level][variant][2])
 
         # fill in the rest of the spaces with 1s
         for row in self.board:
-            while len(row) < self.size:
-                row.insert(random.randrange(0, self.size + 1), 1)
+            while len(row) < size:
+                row.insert(random.randrange(0, size + 1), 1)
 
     def _insert_special(self, value, count):
         for i in range(0, count):
@@ -56,7 +49,7 @@ class GameBoard:
             except ValueError:
                 col = 0
 
-            if len(self.board[row]) < self.size:
+            if len(self.board[row]) < len(self.board):
                 self.board[row].insert(col, value)
             else:
                 i -= 1
@@ -64,3 +57,6 @@ class GameBoard:
 
             if value > 1:
                 self.special_count += 1
+
+    def __str__(self):
+        return str(self.board)
