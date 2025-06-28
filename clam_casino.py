13ac0fa07fd313ce145f468af8a77cd78e378140
kazucroco <kazu@croco.dev>
2025-06-28 19:50:37 -0400

    Add row and col totals to game session

--- clam_casino.py
+++ clam_casino.py
@@ -4,11 +4,14 @@ class ClamCasino:
     level = 0
     score = 0
     board = None
+    row_totals = []
+    col_totals = []
     over = False
     
     def __init__(self, level, size = 5):
         self.level = level
         self.board = GameBoard(self.level, size)
+        self._get_totals()
 
     def flip(self, row, col):
         # cancel the flip if the card is already flipped or game is over
@@ -45,3 +48,39 @@ class ClamCasino:
     def _end_game(self):
         self.over = True
         return self.score
+    
+    def _get_totals(self):
+        col_totals = []
+
+        # every row in the table
+        for i in range(0, len(self.board.board)):
+            row = self.board.board[i]
+            row_point_total = 0
+            row_bomb_count = 0
+
+            # every value in the row
+            for j in range(0, len(row)):
+                val = row[j]
+                # add to the row / col BOMB count
+                if val == 0:
+                    row_bomb_count += 1
+
+                    # creates table for col totals or increases count
+                    if i == 0:
+                        col_totals.append([0, 1])
+                    else:
+                        col_totals[j][1] += 1
+                    
+                # add to the row / col SCORE total
+                else:
+                    row_point_total += val
+
+                    # creates table for col totals or increases count
+                    if i == 0:
+                        col_totals.append([val, 0])
+                    else:
+                        col_totals[j][0] += val
+
+            self.row_totals.append([row_point_total, row_bomb_count])
+            
+        self.col_totals = col_totals
